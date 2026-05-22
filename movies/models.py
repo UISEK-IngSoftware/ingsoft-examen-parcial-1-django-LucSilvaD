51a6ecdce84eccd23cc769bcae23e51efcd47c36
Luciano Silva <luciano.silva@uisek.edu.ec>
2026-05-21 21:00:24 -0400

    Creacion del modelo movie

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    director = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50)
+    release_year = models.IntegerField()
+    synopsis = models.TextField()
+
+    def __str__(self):
+        return self.title
