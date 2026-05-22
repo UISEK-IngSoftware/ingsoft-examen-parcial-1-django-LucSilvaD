0fbffd70558c7a33cb91e545d5387f652f0d7872
Luciano Silva <luciano.silva@uisek.edu.ec>
2026-05-21 21:13:52 -0400

    Creacion de views y configuracion de urls

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    objects = Movie.objects.all()
+    return render(request, 'movies/index.html', {'objects': objects})
+
+def movie_detail(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    return render(request, 'movies/movie_detail.html', {'movie': movie})
+
