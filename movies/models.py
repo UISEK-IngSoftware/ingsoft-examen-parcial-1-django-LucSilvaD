e6f3500e33b91378e5541e2a4e6161ac274d42c6
luciano <luciano.silva@uisek.edu.ec>
2026-05-23 23:47:52 -0400

    style.css -> custom.css - Mejora del diseño visual y creacion del logo

--- movies/models.py
+++ movies/models.py
@@ -13,6 +13,7 @@ class Movie(models.Model):
     genre = models.CharField(max_length=50)
     release_year = models.IntegerField()
     synopsis = models.TextField()
+    poster = models.ImageField(upload_to='posters/', blank=True, null=True)
 
     def __str__(self):
         return self.title
